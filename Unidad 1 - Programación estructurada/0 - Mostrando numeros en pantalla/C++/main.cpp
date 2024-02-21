836443f4d76f6e02b23f78d6e7f466aa9626312e
AlexDominguez18 <alejandrodominguez1823@gmail.com>
2024-02-21 09:49:12 -0600

    unidad 1: mostrando numeros en consola

---
+++ Unidad 1 - Programación estructurada/0 - Mostrando numeros en pantalla/C++/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /**
+     * En el siguiente ejemplo notamos que a diferencia de C, aqui no necesitamos
+     * hacer uso de la funcion 'printf' por lo que solo vamos a mandar lo que queremos
+     * mostrar en pantalla haciendo uso de los operadores '<<' para la salida de datos.
+    */
+    cout << "Mi nombre es Alejandro y mi edad es " << 23 << endl;
+
+    return 0;
+}
---
+++ Unidad 1 - Programación estructurada/0 - Mostrando numeros en pantalla/C/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    /**
+     * En el siguiente ejemplo podemos observar que se hace uso de "%d"
+     * para indicarle al programa que vamos a mostrar un digito numerico.
+     * Posteriormente, le pasamos el numero 23 para que lo muestre en pantalla.
+    */
+    printf("Mi nombre es Alejandro y mi edad es: %d\n", 23);
+
+    return 0;
+}
